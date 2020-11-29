a18b50e40927ea4609ed96d03b3995db64eb624d
Engel, Jan Niklas <jaengel@deloitte.de>
2020-11-29 23:29:15 +0100

    Fix setup.cmd conda commands

--- setup.cmd
+++ setup.cmd
@@ -1,18 +1,21 @@
-@echo off
+@echo on
 
 echo Assuming Anaconda, Windows C++ build tools and Protocol Buffers are installed!
 
 :: use default name for environment if none is provided
 If not "%~1"=="" if "%~2"=="" goto runscript
 set env-name=ObjectDetectionAPI
+goto default
 
 :runscript
 
 :: set environment name
 set env-name=%1
 
+:default
+
 :: create environment and install required packages
-conda create -n %env-name% -y python=3.8
+call conda create -n %env-name% -y python=3.8
 call conda activate %env-name%
 pip install tensorflow==2.3.0 tf_slim opencv-python pycocotools jupyter pywin32==225
 python -c "import tensorflow as tf; print(tf.__version__)"
