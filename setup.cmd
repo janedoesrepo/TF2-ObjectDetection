c28133ebec94d0f20e0b059f9536311cf81c371b
Engel, Jan Niklas <jaengel@deloitte.de>
2020-12-07 19:03:41 +0100

    Add ipykernel to setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,26 +1,29 @@
 @echo off
 
 echo Assuming Anaconda, Windows C++ build tools and Protocol Buffers are installed!
-:: use default name for environment if none is provided
-If not "%~1"=="" if "%~2"=="" goto runscript
-set env-name=TF2-ObjectDetectionAPI
-goto default
 
-:runscript
 :: set environment name
-set env-name=%1
+set env-name=TF2-ObjectDetectionAPI
 
-:default
 :: create environment and install required packages
 call conda create -n %env-name% -y python=3.8
 call conda activate %env-name%
 pip install tensorflow==2.3.0 tf_slim opencv-python pycocotools jupyter pywin32==225
 python -c "import tensorflow as tf; print(tf.__version__)"
-:: install object detection API
+
+:: install nb extensions and register ipykernel
+cmd /c conda install -y nb_conda
+cmd /c python -m ipykernel install --user --name %env-name% --display-name "Python3.8 (%env-name%)"
+
+:: clone tensorflow models repository
 git clone --depth 1 https://github.com/tensorflow/models
 cd models/research
+
 :: Compile protos.
 protoc object_detection/protos/*.proto --python_out=.
+
+:: install object detection API
 pip install .
 cd ../..
+
 echo For Object Detection with your webcam run `python webcam.py`
